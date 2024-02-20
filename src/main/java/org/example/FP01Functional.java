4c40f4ff2aa6403ed51271f951a6cccfb1ace31e
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-02-21 00:06:18 +0530

    print only even no by using structured and filter approach.

--- src/main/java/org/example/FP01Functional.java
+++ src/main/java/org/example/FP01Functional.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class FP01Functional {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(1, 3, 5, 12, 45, 7, 8, 9);
-        printAllNumbersInListFunctional(numbers);
+        //printAllNumbersInListFunctional(numbers);
+        printEvenNumbersInListFunctional(numbers);
     }
 
    /* public static void printNumber(int number){
@@ -15,4 +16,10 @@ public class FP01Functional {
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
          numbers.stream().forEach(System.out::println);
     }
+
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+         numbers.stream().filter(number-> number % 2 == 0).forEach(System.out::println);
+    }
+
+
 }
--- src/main/java/org/example/FP01Structured.java
+++ src/main/java/org/example/FP01Structured.java
@@ -7,11 +7,20 @@ public class FP01Structured {
         // print a element of list in structured approach
         List<Integer> numbers = List.of(1, 3, 5, 12, 45, 7, 8, 9);
 
-        printAllNumbersInListStructured(numbers);
+        //printAllNumbersInListStructured(numbers);
+        printEvenNumbersInListStructured(numbers);
     }
 
     private static void printAllNumbersInListStructured(List<Integer> numbers) {
         for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+    // print only even numbers in structured approch
+    private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+        for (int number : numbers) {
+            if(number % 2 == 0)
             System.out.println(number);
         }
     }

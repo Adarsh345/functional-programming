ca7da9cdd2d0d9c15623dc2e8237607c29278bc8
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-02-21 00:32:46 +0530

    Did exercise 4 that print only even number square by using map interface.

--- src/main/java/org/example/FP01Exercises.java
+++ src/main/java/org/example/FP01Exercises.java
@@ -5,9 +5,13 @@ import java.util.List;
 public class FP01Exercises {
     public static void main(String[] args) {
         List<String> courses = List.of("Spring", "Spring Boot", "API" , "Microservices","AWS", "PCF","Azure", "Docker", "Kubernetes");
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
         //printAllCourses(courses);
        // printSprintCourses(courses);
-        printMinimum3CharacterLengthCourses(courses);
+       // printMinimum3CharacterLengthCourses(courses);
+        printOnlyEvenNoSquare(numbers);
+
 
 
     }
@@ -24,6 +28,13 @@ public class FP01Exercises {
     //exercise 3 print those courses that has the length of minimum 3 characters
     private static void printMinimum3CharacterLengthCourses(List<String> courses) {
         courses.stream().filter(course -> course.length()>=4).forEach(System.out::println);
+    }
+
+    //exercise 4 print only even no square
+    private static void printOnlyEvenNoSquare(List<Integer> numbers) {
+        numbers.stream().filter(number -> number % 2 == 0)
+                .map(number -> number*number)
+                .forEach(System.out:: println);
     }
 
 
